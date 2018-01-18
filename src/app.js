8a52d05cdaaae1a78901c0e931939a558d3b7637
demonk-monk <429513773@qq.com>
2018-01-18 23:45:37 +0800

    能滑动选中item

--- src/app.js
+++ src/app.js
@@ -13,19 +13,31 @@ class App extends React.PureComponent {
         this.onCancel = this.onCancel.bind(this);
     }
     componentDidMount() {
-        let scroll = new BScroll('.picker-scroll', {
-            wheel:{
-                selectedIndex: 0,
-                wheelWrapperClass: 'wheel-scroll',
-                wheelItemClass: 'wheel-item'
-            },
-            probeType: 3
-        });
-        this.wheel = scroll;
-        this.wheel.on('change', () => {
-            this.currentSelectedItemIndex = this.wheel.getSelectedIndex();
-            console.log(this.currentSelectedItemIndex);
-        })
+        this.createWheel();
+    }
+
+    createWheel() {
+      this.wheel = new BScroll(this.wheelEl, {
+          preventDefault: false,
+          // preventDefaultException: [],
+          wheel:{
+              selectedIndex: 0,
+              rotate: 5,
+              adjustTime: 100,
+              wheelWrapperClass: 'wheel-scroll',
+              wheelItemClass: 'wheel-item'
+          }
+      });
+      this.wheel.on('scrollEnd', () => {
+          let currentSelectedItemIndex = this.wheel.getSelectedIndex();
+          if (this.state.currentSelectedItemIndex != currentSelectedItemIndex) {
+              this.setState({
+                  currentSelectedItemIndex
+              })
+          }
+          console.log('scrollEnd detected, currentIndex is: '+currentSelectedItemIndex);
+      });
+      this.wheel.enable();
     }
 
     onCancel() {
@@ -34,6 +46,14 @@ class App extends React.PureComponent {
                 show: !this.state.show
             }
         });
+    }
+
+    onClickItem(index) {
+        console.log('clicked');
+        this.setState({
+            currentSelectedItemIndex: index
+        });
+        this.wheel.wheelTo(index);
     }
     render() {
         return (
@@ -48,17 +68,24 @@ class App extends React.PureComponent {
                                 <div className="picker-title">{ this.props.title || ''}</div>
                                 <div className="picker-confirm">{ this.props.confirmText || '确定'}</div>
                             </div>
-                            <div className="picker-scroll">
+                            <div className="wheel-wrapper">
+                              <div className="wheel" ref={(wheelEl) => this.wheelEl = wheelEl}>
 
-                                <div className="wheel-scroll">
-                                    {
-                                        this.props.data.map((item) => {
-                                            return (
-                                                <div key={item.text} className="wheel-item">{ item.text }</div>
-                                            )
-                                        })
-                                    }
-                                </div>
+                                  <div className="wheel-scroll">
+                                  {
+                                    this.props.data.map((item, index) => {
+                                      return (
+                                        <div
+                                            key={item.text}
+
+                                            className={`wheel-item ${index === this.state.currentSelectedItemIndex ? 'wheel-item-selected': ''}`}>
+                                            <div onClick={(e) => {for (let i in e.target) { console.log(i) } this.onClickItem(index)}}>{ item.text }</div>
+                                          </div>
+                                      )
+                                    })
+                                  }
+                                  </div>
+                              </div>
                             </div>
                         </div>
                     </div>
@@ -81,4 +108,4 @@ const data = [
         value: 2
     },
 ]
-render(<App data={data}/>, document.getElementById('root'));
+render(<App data={data} title="哇哈哈哈"/>, document.getElementById('root'));
